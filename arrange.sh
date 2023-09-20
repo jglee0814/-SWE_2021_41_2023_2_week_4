515249ef980d27c446538c5db0256585eb7c8430
jglee0814 <64621311+jglee0814@users.noreply.github.com>
2023-09-20 20:18:16 +0900

    Update arrange.sh

--- arrange.sh
+++ arrange.sh
@@ -7,6 +7,6 @@ array_upper=({A..Z})
 for index in {0..25}
 do
     dest_folder=${array_lower[$index]}
-    mv ${array_lower[$index]}* $dest_folder/
-    mv ${array_upper[$index]}* $dest_folder/
+    mv files/${array_lower[$index]}* $dest_folder/
+    mv files/${array_upper[$index]}* $dest_folder/
 done
